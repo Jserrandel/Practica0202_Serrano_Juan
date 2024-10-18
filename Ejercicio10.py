7b05dcab20208a561bd49a9c222a8765a0a5b816
Jserrandel <jserrandel@educacion.navarra.es>
2024-10-18 09:12:10 +0200

    Ejercicio 10

---
+++ Ejercicio10.py
@@ -0,0 +1,6 @@
+#Escribir un programa que pregunte por consola
+#por los productos de una cesta de la compra,
+#separados por comas, y muestre por pantalla
+#cada uno de los productos en una línea distinta.
+Cesta = input('¿Que has comprado? Y escribe como una persona civilizada por favor: ')
+print(Cesta.replace(', ','\n'))
